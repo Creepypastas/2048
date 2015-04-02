509e0d4d0548ee1e27b936eabb5c3c518e819760
mautematico <mauricio@navarromiranda.mx>
2015-04-01 18:36:30 -0600

    ¿Qué tal si jugamos con gatos?

---
+++ js/tema.js
@@ -0,0 +1,30 @@
+/* 
+ * Copyright (C) 2015 mautematico
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+images = {
+    2:  "http://lorempixel.com/200/200/cats/DOS/",
+    4:  "http://lorempixel.com/200/200/cats/CUATRO/",
+    8:  "http://lorempixel.com/200/200/cats/OCHO/",
+    16:  "http://lorempixel.com/200/200/cats/DIEZYSEIS/",
+    32:  "http://lorempixel.com/200/200/cats/TREINTAYDOS/",
+    64:  "http://lorempixel.com/200/200/cats/SESENTAYCUATRO/",
+    128:  "http://lorempixel.com/200/200/cats/CientoVeintyOcho/",
+    256:  "http://lorempixel.com/200/200/cats/DoscientosCincuentaYSeis/",
+    512:  "http://lorempixel.com/200/200/cats/QuinientosDoce/",
+    1024:  "http://lorempixel.com/200/200/cats/milVeinticuatro/",
+    2048:  "http://lorempixel.com/200/200/cats/dosmilCuarentayocho/"
+};
